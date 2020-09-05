b17192cb831e37b552279d41d8b6b001a0783035
keatmin <keatmin@gmail.com>
2020-09-05 23:41:34 +0800

    Initial commit

---
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerserviceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
